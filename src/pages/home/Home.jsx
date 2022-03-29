d7d756c36cfbf264f7ed8718401621f06c6c24d9
Fall-InCode <99767884+Fall-InCode@users.noreply.github.com>
2022-03-29 15:42:38 -0400

    Dashboard React
    Routes Working

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import "./home.scss"
+
+const home = () => {
+  return (
+    <div className="home">Home</div>
+  )
+}
+
+export default home
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,9 @@
+import "./login.scss"
+
+const login = () => {
+  return (
+    <div className="login">Login</div>
+  )
+}
+
+export default login
